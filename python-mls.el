6016c780865b3b9dcf90d9452367e0d39bbc1d1f
JD Smith <93749+jdtsmith@users.noreply.github.com>
2022-05-05 11:23:16 -0400

    version bump

--- python-mls.el
+++ python-mls.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/jdtsmith/python-mls
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: languages, processes
-;; Version: 0.1.1
+;; Version: 0.1.2
 
 ;;; Commentary:
 
